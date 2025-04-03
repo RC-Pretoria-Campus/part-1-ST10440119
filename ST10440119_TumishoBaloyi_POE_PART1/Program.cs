03fde4c4c1d3b63f5c890988adb63168a3b36b02
ST10440119 <162671695+ST10440119@users.noreply.github.com>
2025-04-03 13:04:04 +0000

    Phishing, Password Safety, Safe Browsing
    Added more content should the user's input contain one of the following topics in their response, and also improved on the second "welcome" ASCII art to display the user's name as well

--- ST10440119_TumishoBaloyi_POE_PART1/Program.cs
+++ ST10440119_TumishoBaloyi_POE_PART1/Program.cs
@@ -17,17 +17,19 @@ class voiceChat
         String chatName = "Hermit";
         String userName;
 
-        Console.WriteLine("Welcome to cyber security awareness!!!");
+        
+
+        Console.WriteLine("===============================================================================================================");
+
+        Console.WriteLine(FiggleFonts.Standard.Render("     CyberSecurityAwareness"));
+        Console.WriteLine("===============================================================================================================");
         synth.Speak("Welcome, to cyber security awareness, my name is " + chatName);
-        Console.WriteLine("Insert your username: ");
+        Console.WriteLine("Insert your username ");
         synth.Speak("What is your name");
-        userName = Console.ReadLine();
-        Console.WriteLine("=========================================================================================================");
+        Console.WriteLine("Username: ");  userName = Console.ReadLine();
 
         synth.Speak(" Nice to meet you" + userName);
-        Console.WriteLine(FiggleFonts.Standard.Render("     CyberSecurityAwareness"));
-        Console.WriteLine("=========================================================================================================");
-        Console.WriteLine(FiggleFonts.Standard.Render("     Welcome"));
+        Console.WriteLine(FiggleFonts.Standard.Render("     Welcome  " + userName));
         Console.WriteLine("(Type 'EXIT' if you wish to end the conversation.)");
         Console.WriteLine("");
         Console.WriteLine("");
@@ -49,24 +51,54 @@ class voiceChat
             if (user == "How are you?" || user == "how are you" || user == "How are you doing")
             {
                 Console.WriteLine(chatName + ":");
-                Console.WriteLine("I am well, thanks for asking");
-                synth.Speak("I am well, thanks for asking");
+                Console.WriteLine("I am well, thanks for asking, is there something I can help you with regarding cyber security" + userName + "?");
+                synth.Speak("I am well, thanks for asking, is there something I can help you with regarding cyber security " + userName + "?");
 
             }
             else if (user == "What's your purpose?" || user == "What's your purpose" || user == "What is your purpose?")
             {
                 Console.WriteLine(chatName + ":");
-                Console.WriteLine("My purpose is to provide you with more information regarding cyber awareness, I will provide you with information regarding the current topics available in cyber awareness");
-                synth.Speak("My purpose is to provide you with more information regarding cyber awareness, I will provide you with information regarding the current topics available in cyber awareness");
+                Console.WriteLine("My purpose is to provide you with more information regarding cyber awareness.\nI will provide you with information regarding the current topics I have available in cyber awareness");
+                synth.Speak("My purpose is to provide you with more information regarding cyber awareness, I will provide you with information regarding the current topics I have available in cyber awareness");
 
 
             }
             else if (user == "What can I ask you about?" || user == "What can I ask you about?")
             {
                 Console.WriteLine(chatName + ":");
-                Console.WriteLine("You can ask about the topics regarding cyber awareness, the information we cover are from topics related to password safety, phishing, and safe browsing");
-                synth.Speak("You can ask about the topics regarding cyber awareness, the information we cover are from topics related to Password Safety, Phishing, and Safe Browsing");
+                Console.WriteLine("You can ask about the topics regarding cyber awareness, the information I cover are from topics related to: \n\n.Password safety \n.Phishing \n.Safe browsing \n\nWould you like to know more about one of these topics " + userName + "?");
+                synth.Speak("You can ask about the topics regarding cyber awareness, the information I cover are from topics related to Password Safety, Phishing, and Safe Browsing. Would you like to know more about one these topics " + userName + "");
 
+            }
+            else if(user.Contains("Phishing") || user.Contains("phishing"))
+            {
+                Console.WriteLine(chatName + ":");
+                Console.WriteLine("I see you are talking about phishing " + userName + ". Phishing is a cyber crime which aims to steal the user's personal and financial information \nby utilizing false emails, false text messages and false phone calls. \nTips to protect yourself from this crime are as follows: \n\n.Do not share personal information(full names, surnames, ID) \n.Avoid entering unknown links from unknown users \n.Recognize the signs of phishing attacks(Sudden payment requests, unusual links etc.)");
+                Console.WriteLine("");
+                Console.WriteLine("Is there any other topic you would like to know about between: \n\n.Password Safety \n.Safe Browsing");
+                synth.Speak("I see you are talking about phishing " + userName + ". Phishing is a cyber crime which aims to steal the user's personal and financial information by utilizing false emails, false text messages and false phone calls. Tips to protect yourself from this crime are as follows. Do not share personal information such as your full names, surnames, and ID .Avoid entering unknown links from unknown users. Recognize the signs of phishing attacks such as Sudden payment requests, and unusual links as an example");
+                synth.Speak("Is there any other topic you would like to know about between Password Safety, and Safe Browsing?");
+                Console.WriteLine("");
+            }
+            else if(user.Contains("Password safety") || user.Contains("password safety") || user.Contains("Password Safety"))
+            {
+                Console.WriteLine(chatName + ":");
+                Console.WriteLine("I see you are talking about Password Safety " + userName + ". Some tips regarding passsword safety are as follows: \n\n.Ensure your passoword is complex with multiple characters and numbers \n.Do not share your password with anyone regardless if they are close \n.Do not use the same password for each account \n.Reset your password once in a while \n.Make sure no one sees your password");
+                Console.WriteLine("");
+                Console.WriteLine("Is there any other topic you would like to know about between: \n\n.Phishing \n.Safe Browsing");
+                synth.Speak("I see you are talking about Password Safety " + userName + ". Some tips regarding password safety are as follows, Ensure your password is complex with multiple characters and numbers. Do not Share your password with anyone regardless if they are close to you. Do not use the same password for each account. Reset your password once in a while. Make sure no one sees your passwword.");
+                synth.Speak("Is there any other topic you would like to know about between Phishing and Safe Browsing?");
+                Console.WriteLine("");
+            }
+            else if(user.Contains("Safe Browsing") || user.Contains("safe browsing") || user.Contains("Safe browsing"))
+            {
+                Console.WriteLine(chatName + ":");
+                Console.WriteLine("I see you talking about Safe Browsing " + userName + ". Some tips regarding safe browsing are as follows: \n\n.Utilize a safe browser \n.Avoid clicking on ads \n.Avoid clicking on notification pop ups \n.Install a trusted anti-virus software \n.Dont visit websites that don't have an HTTPS");
+                Console.WriteLine("");
+                Console.WriteLine("Is there any other topic you would like to know about between: \n\n.Phishing \n.Password Safety");
+                synth.Speak("I see you are talking about Safe Browsing " + userName + ". Some tips regarding safe browsing are as follows. Utilize a safe browser. Avoid clicking on ads. Avoid clicking on notification pop ups. Install a trusted anti virus software and Dont visit websites that do not contain an HTTPS");
+                synth.Speak("Is there any other topic you would like to know about between Phishing and Password Safety?");
+                Console.WriteLine("");
             }
             else if (user == "")
             {
@@ -79,6 +111,7 @@ class voiceChat
                 loop = false;
                 Console.WriteLine(chatName + ":");
                 Console.WriteLine("Have a good day " + userName + " it was nice chatting with you.");
+                synth.Speak("Have a good day " + userName + ", it was nice chatting with you.");
             }
             else
             {
